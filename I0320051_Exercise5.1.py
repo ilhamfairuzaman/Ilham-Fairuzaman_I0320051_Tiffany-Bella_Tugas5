433438c04c2e4fb34c630a0a533891dc6cdeb86a
Ilham Fairuzaman <ilhamfairuz58@gmail.com>
2021-03-31 18:53:09 +0700

    Exercise

---
+++ I0320051_Exercise5.1.py
@@ -0,0 +1,10 @@
+# Penggunaan if untuk satu kasus
+
+# Input bilangan
+bilangan = int(input('Masukkan bilangan bulat>> '))
+
+# Memeriksa bilangan
+if bilangan % 2 == 0:
+    print('%d adalah bilangan genap' % bilangan)
+
+print()
---
+++ I0320051_Exercise5.2.py
@@ -0,0 +1,12 @@
+# Penggunaan if untuk dua kasus
+
+# Input bilangan
+bilangan = int(input('Masukkan bilangan bulat>> '))
+
+# Memeriksa bilangan
+if bilangan % 2 == 0:
+    print('%d adalah bilangan genap' % bilangan)
+else:
+    print('%d adalah bilangan ganjil' % bilangan)
+
+print()
